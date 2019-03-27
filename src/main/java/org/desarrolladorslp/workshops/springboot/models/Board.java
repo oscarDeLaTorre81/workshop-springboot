7c673cec5667312d2a6052e3c947a23c29af8fd5
Oscar <delatorreOscar81@gmail.com>
2019-03-26 19:23:01 -0600

    El Oscar

--- src/main/java/org/desarrolladorslp/workshops/springboot/models/Board.java
+++ src/main/java/org/desarrolladorslp/workshops/springboot/models/Board.java
@@ -41,6 +41,20 @@ public class Board {
         return name;
     }
 
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        if (!super.equals(object)) return false;
+        Board board = (Board) object;
+        return id == board.id &&
+                java.util.Objects.equals(name, board.name) &&
+                java.util.Objects.equals(user, board.user);
+    }
+
+    public int hashCode() {
+        return java.util.Objects.hash(super.hashCode(), id, name, user);
+    }
+
     public void setName(String name) {
         this.name = name;
     }
